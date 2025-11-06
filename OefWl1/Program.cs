fe34199980ff98552b5f0af4363aab38a68db3cc
Maxim-Bauters-PXL <12500634@student.pxl.be>
2025-11-06 13:32:25 +0100

    DEMO - 01

---
+++ OefWl1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Oefening
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gebruikersnaam: ");
+            string nameInput = Console.ReadLine();
+        }
+    }
+}
